725c19515061db718e5ffa99aa0f207fc372bd62
Arpit-Pathak14 <92110146+Arpit-Pathak14@users.noreply.github.com>
2021-10-14 14:10:33 +0530

    Add files via upload
    Operations in python
    Any one of can contribute to this by adding new !

---
+++ operations.py
@@ -0,0 +1,36 @@
+'''
+You can remove # or comment by ctrl + / or simply remove br crusor 
+You can also edit these codes according to your will
+You can also convert the input from into to any format  such as float, etc.
+'''    
+    # Adding two numbers
+#Defing a value or a variable
+# a = int(input("Enter your value: "))
+# b = int(input("Enter your another number"))
+# c = a + b
+# print("Your sum is : ", (c))
+
+# Substracting the number
+# Defing the another value
+# e = int(input("Enter your value: "))
+# f = int(input("Enter your another number: "))
+# g = e-f or f-e 
+# print("Your subtract number: ", g)
+
+# Divison in python 
+# dividend = int(input("Enter your dividend here: "))
+# divisor = int(input("Enter your divisor here: "))
+# reminder = dividend / divisor
+# print("Your result or remainder is : " , reminder)
+
+#Multiplication in python
+# num1 = int(input("Enter your first number here: "))
+# num2 = int(input("Enter your secound number here"))
+# result = num1 * num2 
+# print("Your result is:  ", result)
+
+
+'''
+Happy coding!
+
+'''
